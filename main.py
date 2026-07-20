10316a9474b2b7ea987d2ab1c9951ae4f33f134a
1986sherik-max <1986sherik@gmail.com>
2026-07-21 03:26:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ OWNER_ID = 711427177
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# Faqat bir marta xabar yuborish uchun
+KNOWN_USERS = set()
+KNOWN_GROUPS = set()
+
 # Linklarni aniqlash uchun
 LINK_PATTERN = re.compile(
     r"(https?://|www\.|t\.me|telegram\.me|@\w+|[a-zA-Z0-9_-]+\.(ru|com|uz))",
@@ -40,11 +44,37 @@ BLACKLIST = [
 async def start(message: Message):
     await message.answer("🛡 Moderator Bot ishga tushdi!")
 
+    if message.chat.type == "private":
+        if message.from_user.id not in KNOWN_USERS:
+            KNOWN_USERS.add(message.from_user.id)
+
+            await bot.send_message(
+                OWNER_ID,
+                f"""🆕 Yangi foydalanuvchi
+
+👤 Ism: {message.from_user.full_name}
+🆔 ID: {message.from_user.id}
+📛 Username: @{message.from_user.username if message.from_user.username else "yo'q"}"""
+            )
+
+
 @dp.message(F.text)
 async def check_links(message: Message):
     # Guruhdan tashqarida ishlamasin
     if message.chat.type not in ("group", "supergroup"):
         return
+
+    # Yangi guruh haqida faqat bir marta xabar berish
+    if message.chat.id not in KNOWN_GROUPS:
+        KNOWN_GROUPS.add(message.chat.id)
+
+        await bot.send_message(
+            OWNER_ID,
+            f"""🆕 Bot yangi guruhga qo'shildi
+
+👥 Guruh: {message.chat.title}
+🆔 Chat ID: {message.chat.id}"""
+        )
 
     # Adminlarni tekshirish
     member = await bot.get_chat_member(message.chat.id, message.from_user.id)
@@ -79,8 +109,10 @@ async def check_links(message: Message):
         await asyncio.sleep(5)
         await msg.delete()
 
+
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
